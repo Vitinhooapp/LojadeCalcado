a587e43bc6dc7251b9216459397841de8dbdd456
Victor Ribeiro <92072647+Vitinhooapp@users.noreply.github.com>
2023-09-06 08:01:47 -0300

    Update Compra.java

--- src/main/java/com/mycompany/lojadecalcados/Compra.java
+++ src/main/java/com/mycompany/lojadecalcados/Compra.java
@@ -2,6 +2,6 @@ package com.mycompany.lojadecalcados;
 
 public class Compra {
     
-    Cliente cliente1 = new Cliente("Rafael","rafaelmatadordeporco@gmail.com",40028922,"Rua aquela lá, numero 69, bairro dos noia");
+    Cliente cliente1 = new Cliente("Rafael","rafaelmatadordeporco@gmail.com",40028922,"Rua aquela lá, numero 69, bairro jardim Petropolis");
     
 }
